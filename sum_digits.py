f814b08cd3635db0282f5ada07f592ba76b51b88
stil-e <brian.ikubu@strathmore.edu>
2025-04-26 11:31:23 +0300

    Update sum_digits.py
    simplified some code

--- sum_digits.py
+++ sum_digits.py
@@ -7,7 +7,7 @@ def sum_of_digits(number):
     number = abs(number)
     total = 0
     while number > 0:
-        total += number % 10
+        total= total + number % 10
         number //= 10
     return total
 
