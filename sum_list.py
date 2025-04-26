ff42d7f88533b2fb5c1a78d6407d248f1a50d1f8
stil-e <brian.ikubu@strathmore.edu>
2025-04-26 11:29:30 +0300

    Update sum_list.py
    shortened some code

--- sum_list.py
+++ sum_list.py
@@ -1,9 +1,9 @@
 # function to sum all elements in a list
 
 def sum_of_list (numbers):
-    """ 
-    takes a list of numbers and returns their sum. 
-    """
+
+    #takes a list of numbers and returns their sum. 
+
     total = 0
     for num in numbers:
         total+=num
@@ -13,6 +13,6 @@ def sum_of_list (numbers):
     #tests
 if __name__ == "__main__":
         print(sum_of_list([1,2,3,4,5,6]))
-        #should print 21 which is the sum of the numbers
-        print(sum_of_list([20,-2,3])) #should print 21
-        print(sum_of_list([])) #should print 0
+        #prints 21
+        print(sum_of_list([20,-2,3])) #prints 21
+        print(sum_of_list([])) #prints 0
